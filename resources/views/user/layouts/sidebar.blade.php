0ca95741479f05981348c52dc70c911918930150
Adden07 <anasrajput3@gmail.com>
2022-02-01 01:34:29 +0500

    work

---
+++ resources/views/user/layouts/sidebar.blade.php
@@ -0,0 +1,146 @@
+<nav class="sidebar">
+    <div class="logo d-flex justify-content-between">
+        <a href="index.html"><img src="img/logo.png" alt=""></a>
+        <div class="sidebar_close_icon d-lg-none">
+            <i class="ti-close"></i>
+        </div>
+    </div>
+    <ul id="sidebar_menu">
+        <li class="mm-active">
+          <a class="has-arrow"  href="#"  aria-expanded="false">
+          <!-- <i class="fas fa-th"></i> -->
+          <img src="img/menu-icon/dashboard.svg" alt="">
+            <span>Dashboard</span>
+          </a>
+          <ul>
+            <li><a class="active" href="index.html">Directory</a></li>
+            <li><a  href="index_2.html">Default</a></li>
+           
+          </ul>
+
+        </li>
+        <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <img src="img/menu-icon/2.svg" alt="">
+              <span>My Profile</span>
+            </a>
+            <ul>
+              <li><a href="login.html">Login</a></li>
+              <li><a href="resister.html">Register</a></li>
+              <li><a href="error_400.html">Error 404</a></li>
+              <li><a href="error_500.html">Error 500</a></li>
+              <li><a href="forgot_pass.html">Forgot Password</a></li>
+              <li><a href="gallery.html">Gallery</a></li>
+            </ul>
+        </li>
+
+        <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <img src="img/menu-icon/2.svg" alt="">
+              <span>Cash Box</span>
+            </a>
+            <ul>
+              <li><a href="login.html">Login</a></li>
+              <li><a href="resister.html">Register</a></li>
+              <li><a href="error_400.html">Error 404</a></li>
+              <li><a href="error_500.html">Error 500</a></li>
+              <li><a href="forgot_pass.html">Forgot Password</a></li>
+              <li><a href="gallery.html">Gallery</a></li>
+            </ul>
+        </li>
+
+        <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <img src="img/menu-icon/2.svg" alt="">
+              <span>Deposits</span>
+            </a>
+            <ul>
+              <li><a href="login.html">Login</a></li>
+              <li><a href="resister.html">Register</a></li>
+              <li><a href="error_400.html">Error 404</a></li>
+              <li><a href="error_500.html">Error 500</a></li>
+              <li><a href="forgot_pass.html">Forgot Password</a></li>
+              <li><a href="gallery.html">Gallery</a></li>
+            </ul>
+        </li>
+
+        <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <img src="img/menu-icon/2.svg" alt="">
+              <span>My Profit</span>
+            </a>
+            <ul>
+              <li><a href="login.html">Login</a></li>
+              <li><a href="resister.html">Register</a></li>
+              <li><a href="error_400.html">Error 404</a></li>
+              <li><a href="error_500.html">Error 500</a></li>
+              <li><a href="forgot_pass.html">Forgot Password</a></li>
+              <li><a href="gallery.html">Gallery</a></li>
+            </ul>
+          </li>
+
+          <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <img src="img/menu-icon/2.svg" alt="">
+              <span>My Bonus</span>
+            </a>
+            <ul>
+              <li><a href="login.html">Login</a></li>
+              <li><a href="resister.html">Register</a></li>
+              <li><a href="error_400.html">Error 404</a></li>
+              <li><a href="error_500.html">Error 500</a></li>
+              <li><a href="forgot_pass.html">Forgot Password</a></li>
+              <li><a href="gallery.html">Gallery</a></li>
+            </ul>
+        </li>
+
+        <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <img src="img/menu-icon/2.svg" alt="">
+              <span>Withdraw</span>
+            </a>
+            <ul>
+              <li><a href="login.html">Login</a></li>
+              <li><a href="resister.html">Register</a></li>
+              <li><a href="error_400.html">Error 404</a></li>
+              <li><a href="error_500.html">Error 500</a></li>
+              <li><a href="forgot_pass.html">Forgot Password</a></li>
+              <li><a href="gallery.html">Gallery</a></li>
+            </ul>
+        </li>
+
+        <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <img src="img/menu-icon/2.svg" alt="">
+              <span>Tickets</span>
+            </a>
+            <ul>
+              <li><a href="login.html">Login</a></li>
+              <li><a href="resister.html">Register</a></li>
+              <li><a href="error_400.html">Error 404</a></li>
+              <li><a href="error_500.html">Error 500</a></li>
+              <li><a href="forgot_pass.html">Forgot Password</a></li>
+              <li><a href="gallery.html">Gallery</a></li>
+            </ul>
+        </li>
+
+
+        <li class="">
+            <a   class="has-arrow" href="#" aria-expanded="false">
+              <img src="img/menu-icon/2.svg" alt="">
+              <span>Referals</span>
+            </a>
+            <ul>
+              <li><a href="login.html">Login</a></li>
+              <li><a href="resister.html">Register</a></li>
+              <li><a href="error_400.html">Error 404</a></li>
+              <li><a href="error_500.html">Error 500</a></li>
+              <li><a href="forgot_pass.html">Forgot Password</a></li>
+              <li><a href="gallery.html">Gallery</a></li>
+            </ul>
+          </li>
+
+
+      </ul>
+    
+</nav>
